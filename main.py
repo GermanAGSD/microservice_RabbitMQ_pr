8e19c58d3c13d391b30c50062cf94f8837322931
Yandex Student <student@yandex.ru>
2025-12-29 03:24:06 +0300

    start

--- main.py
+++ main.py
@@ -1,19 +1,39 @@
 from fastapi import FastAPI
 from faststream.rabbit.fastapi import RabbitRouter
 import uvicorn
-
+import random
 
 app = FastAPI()
 
 router = RabbitRouter("amqp://admin:admin123@172.30.30.19:5672/")
 
 @router.get("/order")
-async def make_order(name: str):
+async def make_order(name: str, event: str):
+
+    data_json = {
+        "id": random.randint(1, 1_000_000),
+        "event": event,
+        "message": name
+    }
     await router.broker.publish(
-        f"new order: {name}",
+        data_json,
         queue="orders"
     )
     return {"data": "success"}
+
+@router.get("/product")
+async def new_product(name: str, event: str):
+    data_json = {
+        "id": random.randint(1, 1_000_000),
+        "event": event,
+        "message": name
+    }
+    await router.broker.publish(
+        data_json,
+        queue="product"
+    )
+    return {"data": "success"}
+
 
 app.include_router(router)
 
